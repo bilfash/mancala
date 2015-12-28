7600374bf8a57e6d6ae58965df947c2993a91d46
divijaya <muhammad.divi.jaya.nuryanto13@mhs.if.its.ac.id>
2015-12-29 06:31:59 +0700

    nyoba commot
    asdadasdas

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import thread
 import pickle
 
 input_socket = []
-server_address = ('localhost', 5000)
+server_address = ('10.151.63.115', 5000)
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(server_address)
